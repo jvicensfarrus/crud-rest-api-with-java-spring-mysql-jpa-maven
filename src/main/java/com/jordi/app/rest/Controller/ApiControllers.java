78b12ea821268b5b91c9c7e918b8cebf77726627
Jordi Vicens <jvicensfarrus@gmail.com>
2023-08-01 12:26:16 +0200

    Update and Delete methods; Postman collection update;

--- src/main/java/com/jordi/app/rest/Controller/ApiControllers.java
+++ src/main/java/com/jordi/app/rest/Controller/ApiControllers.java
@@ -3,8 +3,11 @@ package com.jordi.app.rest.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +34,25 @@ public class ApiControllers {
 	public String saveUser(@RequestBody User user) {
 		userRepo.save(user);
 		return "Saved...";
+	}
+	
+	@PutMapping(value = "update/{id}")
+	public String updateUser(@PathVariable long id, @RequestBody User user) {
+		User updatedUser = userRepo.findById(id).get();
+		updatedUser.setFirstName(user.getFirstName());
+		updatedUser.setLastName(user.getLastName());
+		updatedUser.setOccupation(user.getOccupation());
+		updatedUser.setAge(user.getAge());
+		userRepo.save(updatedUser);
+		return "Updated...";
+		
+	}
+	
+	@DeleteMapping(value = "/delete/{id}")
+	public String deleteUser(@PathVariable long id) {
+		User deleteUser = userRepo.findById(id).get();
+		userRepo.delete(deleteUser);
+		return "Deleted...";
 	}
 
 }
